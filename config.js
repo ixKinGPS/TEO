c9d38814b7f26c36402ece971388d403c895f8d9
ixKinGPS <70635613+ixKinGPS@users.noreply.github.com>
2020-09-02 12:50:48 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NzQ5ODc2NjYyMjk2MTE3MjQ4.X0yXEA.CyGKE__JeL97w_QcEOwG7NLrBPI', /// توكن البوت
+    YT_API_KEY: 'api ', /// مش مهم
+    prefix: '1', // هنا برفكس او مقدمة البوت
+    devs: ['692735916862603366'] //// ايدي حسابك
+}
